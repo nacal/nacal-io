d74e2492194467eb09444831256c96e2f18c1b40
nkthkr <hikaru.nakata@nislab.doshisha.ac.jp>
2021-12-16 17:37:39 +0900

    develop

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import './App.scss'
 
+import { FaGithubSquare, FaTwitterSquare } from 'react-icons/fa'
+
 const App = () => {
   return (
     <>
@@ -11,6 +13,22 @@ const App = () => {
       </head>
       <main>
         <h1 className='title'>nacal.io</h1>
+        <div className='contents'>
+          <div>
+            <div className='links'>
+              <a href='https://github.com/nacal' className='icon'>
+                <FaGithubSquare />
+              </a>
+              <a href='https://twitter.com/_nacal' className='icon'>
+                <FaTwitterSquare />
+              </a>
+            </div>
+            <h2 className='copy'>Under development.</h2>
+            <address className='address'>
+              <a href='mailto:nacal.dev@gmail.com'>nacal.dev@gmail.com</a>
+            </address>
+          </div>
+        </div>
       </main>
     </>
   )
